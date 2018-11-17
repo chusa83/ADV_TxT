db1edf6e26f15a6b42bf723df026bf7fb929d1f2
chusa83 <chusa83@gmail.com>
2018-11-17 16:48:42 -0500

    Simplificación de la física
    Simplificación de la física del meteoro con la dreación del script weigthless

--- assets/Asteroides/Meteoro.gd
+++ assets/Asteroides/Meteoro.gd
@@ -1,29 +1,37 @@
-extends RigidBody2D
+extends "res://Weightless.gd"
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-onready var estrofa = get_node("Estrofa")
-onready var viewport = get_viewport().get_visible_rect().size
+var Pedazo11 = preload("res://assets/Asteroides/Pedazos/Meteoro1-1.tscn")
+var Pedazo12 = preload("res://assets/Asteroides/Pedazos/Meteoro1-2.tscn")
+var Pedazo13 = preload("res://assets/Asteroides/Pedazos/Meteoro1-3.tscn")
+var Pedazo14 = preload("res://assets/Asteroides/Pedazos/Meteoro1-4.tscn")
+var Pedazo15 = preload("res://assets/Asteroides/Pedazos/Meteoro1-5.tscn")
+var Pedazo16 = preload("res://assets/Asteroides/Pedazos/Meteoro1-6.tscn")
+var Pedazo17 = preload("res://assets/Asteroides/Pedazos/Meteoro1-7.tscn")
+var Pedazo18 = preload("res://assets/Asteroides/Pedazos/Meteoro1-8.tscn")
+var Pedazo19 = preload("res://assets/Asteroides/Pedazos/Meteoro1-9.tscn")
+
+
+signal explode
+
+enum Meteoro {
+	UNO, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, OCHO
+}
+
+export(Meteoro) var meteoro = Meteoro.UNO
+
+var radius = get_node("Estrofa").texture.get_width() /2 * get_node("Estrofa").scale
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	connect("explode", self, "_explode")
 	pass
 
-func _integrate_forces(state):
-	var size = estrofa.texture.get_size() * estrofa.scale
-	var trans = state.get_transform()
-	if (trans.origin.x < -size.x/2):
-		trans.origin.x += viewport.x + size.x
-	elif (trans.origin.x > viewport.x + size.x/2):
-		trans.origin.x -= viewport.x + size.x
-	elif(trans.origin.y < -size.y/2):
-		trans.origin.y += viewport.y + size.y
-	elif (trans.origin.y > viewport.y + size.y/2):
-		trans.origin.y -= viewport.y + size.y
-		
-	state.set_transform(trans)
+
+func _explode():
+	
+	queue_free()
 	pass
 
 #func _process(delta):
