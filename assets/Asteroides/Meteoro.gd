6fab13217c1702788cef02181ed5fccac7c96a9f
chusa83 <chusa83@gmail.com>
2018-11-20 08:02:57 -0500

    Intento de instanciar dinámicamente

--- assets/Asteroides/Meteoro.gd
+++ assets/Asteroides/Meteoro.gd
@@ -1,15 +1,6 @@
 extends "res://Weightless.gd"
-
-var Pedazo11 = preload("res://assets/Asteroides/Pedazos/Meteoro1-1.tscn")
-var Pedazo12 = preload("res://assets/Asteroides/Pedazos/Meteoro1-2.tscn")
-var Pedazo13 = preload("res://assets/Asteroides/Pedazos/Meteoro1-3.tscn")
-var Pedazo14 = preload("res://assets/Asteroides/Pedazos/Meteoro1-4.tscn")
-var Pedazo15 = preload("res://assets/Asteroides/Pedazos/Meteoro1-5.tscn")
-var Pedazo16 = preload("res://assets/Asteroides/Pedazos/Meteoro1-6.tscn")
-var Pedazo17 = preload("res://assets/Asteroides/Pedazos/Meteoro1-7.tscn")
-var Pedazo18 = preload("res://assets/Asteroides/Pedazos/Meteoro1-8.tscn")
-var Pedazo19 = preload("res://assets/Asteroides/Pedazos/Meteoro1-9.tscn")
-
+var numPedazos = [9,9,8,7,8,4,7,9]
+var pedazos ={}
 
 signal explode
 
@@ -26,14 +17,35 @@ func _ready():
 	# Initialization here
 	connect("explode", self, "_explode")
 	radius = (get_node("Estrofa").texture.get_width() /2) * get_node("Estrofa").scale
+	#For para declarar los múltiples pedazos
+	for each in numpedazos:
+		for i in range (1, numpedazos[each]):
+			pedazos = {"name" : each, "trozo": i, path: str("res://assets/Asteroides/Pedazos/Meteoro"+each+"-"+i+".tscn")}
+		
+	
 	pass
 
 
 func _explode():
-	if meteoro == Meteoro.UNO:
-		var asteroid
-		
-		
+	var asteroid
+	match meteoro:
+		UNO:
+			asteroid = Pedazo11.instance()
+		DOS:
+			
+		TRES:
+			
+		CUATRO:
+			
+		CINCO:
+			
+		SEIS:
+			
+		SIETE:
+			
+		OCHO:
+			
+	queue_free()
 	pass
 
 #func _process(delta):
