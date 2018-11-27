9fd2e813e782d091a5e69f2e5516a5c5d1063fcb
chusa83 <chusa83@gmail.com>
2018-11-27 11:08:33 -0500

    NIVELES
    Programación de niveles y carga de meteoros según el nivel.

--- Game.gd
+++ Game.gd
@@ -3,25 +3,10 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var cantidadMeteoros = 1
 # dificultaf = tiempo + velocidadMeteoro
 #export(String) scene_To_Load
 
 func _ready():
-	#Metodo para lograr cargar vectores de velocidades aleatoreos en
-	#cada uno de los meteoros
-	for i in range(1,cantidadMeteoros):
-		var meteoro = get_node(str("Meteoro",i))
-		#get_node("Meteoro8").set_linear_velocity()
-		meteoro.Meteoro = UNO
-		var vel = Vector2(0,0)
-		randomize()
-		vel.x = rand_range(-50,50)
-		randomize()
-		vel.y = rand_range(-50,50)
-		meteoro.set_linear_velocity(vel)
-		
-	#$SceneTreeTimer.connect("timeout", self, "_destroy_meteoro")
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
